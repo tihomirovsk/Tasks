ea52d4a6e87cde064d5309c972919593d4c8d417
tihomirovsk <pesokote@gmail.com>
2022-09-18 21:18:43 +0300

    Task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число 1 : ");
+string input1 = Console.ReadLine();
+int number1 = int.Parse(input1);
+
+Console.WriteLine("Введите число 2 : ");
+string input2 = Console.ReadLine();
+int number2 = int.Parse(input2);
+
+int max = 0;
+
+{
+    while (number2 < number1);
+ 
+        max = number1;
+        Console.WriteLine(max = number1);
+
+    if (number1 < number2);
+
+        max = number2;
+        Console.WriteLine(max = number2);
+}
