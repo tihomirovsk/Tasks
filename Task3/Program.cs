e45c35f330fe9b5d6cb3a56d1d0acb6e3f736e55
tihomirovsk <pesokote@gmail.com>
2022-09-18 21:25:41 +0300

    Task3 done

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число : ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+int odd = 1;
+int even = 2;
+
+
+if (number / even == even )
+{
+    Console.WriteLine("Чётное");
+}
+else
+{
+    Console.WriteLine("Нечётное");
+}
