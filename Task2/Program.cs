72ae5d3537c3dffae5e21c31edc359a342e91ab1
tihomirovsk <pesokote@gmail.com>
2022-09-18 21:22:26 +0300

    Task2 done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число 1 : ");
+string input1 = Console.ReadLine();
+int number1 = int.Parse(input1);
+
+Console.WriteLine("Введите число 2 : ");
+string input2 = Console.ReadLine();
+int number2 = int.Parse(input2);
+
+Console.WriteLine("Введите число 3 : ");
+string input3 = Console.ReadLine();
+int number3 = int.Parse(input3);
+
+int max = 0;
+
+{
+    while (max < number1);
+ 
+        max = number1;
+
+    if (max < number2);
+
+        max = number2;
+
+    if (max < number3);
+
+        max = number3;
+Console.WriteLine(max);
+}
